faa2794bdc7a4e36e4cecba3443fad343d3ce172
ABHINAV RAI <Lyghtjr@github.com>
2021-03-21 22:14:40 +0530

    Updates

---
+++ precache-manifest.f5665e6fe8f59daa9b39c37588d3a9d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d34a4aaa70f9999c454964bd4c6e115",
+    "url": "/TO_DO_WEB/index.html"
+  },
+  {
+    "revision": "740fbf5e114c3261c537",
+    "url": "/TO_DO_WEB/static/css/main.11d7e628.chunk.css"
+  },
+  {
+    "revision": "60b81e7651a93be96190",
+    "url": "/TO_DO_WEB/static/js/2.c30c7e95.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/TO_DO_WEB/static/js/2.c30c7e95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "740fbf5e114c3261c537",
+    "url": "/TO_DO_WEB/static/js/main.b81e18aa.chunk.js"
+  },
+  {
+    "revision": "b2a0fb40866b13d65012",
+    "url": "/TO_DO_WEB/static/js/runtime-main.7d589e20.js"
+  }
+]);
